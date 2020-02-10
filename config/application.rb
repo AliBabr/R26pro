4d58d4704ad1c26ca247aaf7351870c8edc4f022
alibabr <babar2166@gmail.com>
2020-02-11 01:44:11 +0500

    added other routes cros protection

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module R26pro
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins "*"
-        resource "*", headers: :any, methods: [:get, :post, :options]
+        resource "*", headers: :any, methods: [:get, :post, :delete, :put, :patch, :options]
       end
     end
 
